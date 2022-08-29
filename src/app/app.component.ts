468a503dad0562ec4f4d2cc0e3da8047c1f023e9
Nahuel Rosas <33013514+nahuelRosas@users.noreply.github.com>
2022-08-29 05:42:24 -0300

    Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'simpleCalculatorApp';
+  operatorX: number = 0;
+  operatorY: number = 0;
+  result: number = 0;
+  sum(): void {
+    this.result = this.operatorX + this.operatorY;
+  }
 }
